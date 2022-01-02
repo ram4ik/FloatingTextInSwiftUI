9f0d30eb5d211191831eab816e79a8a91f50b767
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-01-02 20:22:35 +0200

    Initial Commit

---
+++ FloatingTextInSwiftUI/FloatingTextInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTextInSwiftUIApp.swift
+//  FloatingTextInSwiftUI
+//
+//  Created by ramil on 02.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTextInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
